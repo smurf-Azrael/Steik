ca6a0fd8922f466fd42401610dc2943fecaf7995
smurf-Azrael <luisvelasco00117@gmail.com>
2024-03-19 16:12:20 -0300

    fix: navbar issue, token show isses

--- components/common/nft-container.tsx
+++ components/common/nft-container.tsx
@@ -1,10 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import Image from "next/image"
 import { CheckCircle } from "lucide-react"
-
-import NFT from "../utils/nfts"
 
 interface NFTContainerProps {
   id: number
